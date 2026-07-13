145bde0fba817f0a77ae4276580e7b14ee1ca094
ruben-deve <ruben202616@gmail.com>
2026-07-13 12:00:44 +0100

    modal echange

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+const ouvrirModalBtn = document.getElementById('ouvrirModal');
+const fermerModalBtn = document.getElementById('fermerModal');
+const modalOverlay = document.getElementById('modalOverlay');
+const formEchange = document.getElementById('formEchange');
+
+// ouverture
+ouvrirModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('modal-visible');
+});
+
+// fermeture (croix)
+fermerModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.remove('modal-visible');
+});
+
+// fermeture clic en dehors 
+modalOverlay.addEventListener('click', () => {
+    if(event.target === modalOverlay) {
+        modalOverlay.classList.remove('modal-visible');
+    }
+});
+
+// soumission form
+formEchange.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("Échane effectué (PHP à venir");
+    modalOverlay.classList.remove('modal-visible');
+
+})
